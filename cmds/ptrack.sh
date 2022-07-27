6f1fda98470c767137c71d43bf29208dbdccdbf4
changtimwu <changtimwu@ieiworld.com>
2022-07-27 10:32:17 +0800

    enable frequently used options by default

--- cmds/ptrack.sh
+++ cmds/ptrack.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-python deploy/pphuman/pipeline.py --config deploy/pphuman/config/infer_cfg.yml --video_file=$1 --output_dir=$OUTPUTDIR --device=gpu --model_dir det=ppyoloe
+othopts="--do_entrance_counting  --draw_center_traj"
+python deploy/pphuman/pipeline.py --config deploy/pphuman/config/infer_cfg.yml --video_file=$1 --output_dir=$OUTPUTDIR --device=gpu $othopts --model_dir det=ppyoloe
