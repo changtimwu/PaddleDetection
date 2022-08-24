d79d418d35ef7420de982be1471a528530c3f3c9
changtimwu <changtimwu@ieiworld.com>
2022-08-24 09:58:14 +0800

    enable entrance line counting

--- cmds/pattract.sh
+++ cmds/pattract.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python deploy/pphuman/pipeline.py --config deploy/pphuman/config/infer_cfg.yml --video_file=$1 --output_dir=$OUTPUTDIR --device=gpu --enable_attr=True --enable_action=True 
+python deploy/pphuman/pipeline.py --config deploy/pphuman/config/infer_cfg.yml --video_file=$1 --output_dir=$OUTPUTDIR --device=gpu --enable_attr=True --enable_action=True --do_entrance_counting 
